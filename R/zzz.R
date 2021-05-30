6886ba0ed9eb9a95cf4c3cc0e2c5d8ca792ac2b6
Eric Lamphere <ericjlamphere@gmail.com>
2021-05-29 20:13:55 -0400

    bug fix

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onAttach <- function(...) {
   packageStartupMessage(
     emo::ji("beers"),
-    crayon::white(" ezxfig "),
-    crayon::cyan(packageVersion("ezxfig"))
+    crayon::white(" ezexplore "),
+    crayon::cyan(packageVersion("ezexplore"))
   )
 }
